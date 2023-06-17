9c017c3e85fdc03cb9bcaa5976a8cf6b9ca2fb5b
Андрей <martynkevich.2000@gmail.com>
2023-06-18 02:36:36 +0300

    fix

--- project/src/js/app.js
+++ project/src/js/app.js
@@ -1,10 +1,9 @@
 const theme = document.querySelector('.theme-switch')
-theme.addEventListener('click', () => {
-	toggleTheme()
-	console.log('g')
-})
 function toggleTheme() {
 	document.body.hasAttribute('light')
 		? document.body.removeAttribute('light')
 		: document.body.setAttribute('light', '')
 }
+theme.addEventListener('click', () => {
+	toggleTheme()
+})
