f69a8640a7cdd4aa4d1f62c3eb224db5f1031078
Андрей <martynkevich.2000@gmail.com>
2023-06-18 02:48:56 +0300

    js

--- project/src/js/app.js
+++ project/src/js/app.js
@@ -1,9 +1,9 @@
-const theme = document.querySelector('.theme-switch')
-function toggleTheme() {
-	document.body.hasAttribute('light')
-		? document.body.removeAttribute('light')
-		: document.body.setAttribute('light', '')
-}
-theme.addEventListener('click', () => {
-	toggleTheme()
-})
+// const theme = document.querySelector('.theme-switch')
+// function toggleTheme() {
+// 	document.body.hasAttribute('light')
+// 		? document.body.removeAttribute('light')
+// 		: document.body.setAttribute('light', '')
+// }
+// theme.addEventListener('click', () => {
+// 	toggleTheme()
+// })
