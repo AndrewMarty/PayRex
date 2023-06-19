c832519eae4d54cc9e4e8e630317e02c18d2376f
Андрей <martynkevich.2000@gmail.com>
2023-06-19 19:43:53 +0300

    scroll

--- project/src/js/app.js
+++ project/src/js/app.js
@@ -3,7 +3,33 @@ const burger = document.querySelector('.burger')
 const headerMenu = document.querySelector('.header__menu')
 const map = document.querySelector('.map')
 const mapPoints = document.querySelectorAll('.map__item')
-
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+	animationTime = 500,
+	framesCount = 144
+anchors.forEach(function (item) {
+	item.addEventListener('click', function (e) {
+		e.preventDefault()
+		console.log(window.innerHeight)
+		let coordY =
+			document.querySelector(item.getAttribute('href')).getBoundingClientRect()
+				.top + window.scrollY
+		let scroller = setInterval(function () {
+			let scrollBy = coordY / framesCount
+			if (
+				scrollBy > window.scrollY - coordY &&
+				window.innerHeight + window.scrollY < document.body.offsetHeight
+			) {
+				window.scrollBy(0, scrollBy)
+			} else {
+				window.scrollTo(0, coordY)
+				clearInterval(scroller)
+			}
+		}, animationTime / framesCount)
+	})
+})
+if (localStorage.getItem('theme') === 'light') {
+	toggleTheme()
+}
 map.addEventListener('click', event => {
 	const target = event.target
 	if (target.closest('.map__item')) {
@@ -44,12 +70,14 @@ function toggleMenu() {
 }
 function toggleTheme() {
 	if (document.body.hasAttribute('light')) {
+		localStorage.removeItem('theme')
 		document.body.removeAttribute('light')
 		map.classList.remove('map_light')
 		switchers.forEach(switcher => {
 			switcher.classList.remove('theme-light')
 		})
 	} else {
+		localStorage.setItem('theme', 'light')
 		document.body.setAttribute('light', '')
 		map.classList.add('map_light')
 		switchers.forEach(switcher => {
