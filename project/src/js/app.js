a854369e3ea46deb76957b117a94532272a1f449
Андрей <martynkevich.2000@gmail.com>
2023-06-19 03:03:28 +0300

    add

--- project/src/js/app.js
+++ project/src/js/app.js
@@ -1,12 +1,36 @@
-const theme = document.querySelector('.theme-switch')
-function toggleTheme() {
-	document.body.hasAttribute('light')
-		? document.body.removeAttribute('light')
-		: document.body.setAttribute('light', '')
-}
-theme.addEventListener('click', () => {
-	toggleTheme()
+const switchers = document.querySelectorAll('.theme-switch')
+const burger = document.querySelector('.burger')
+const headerMenu = document.querySelector('.header__menu')
+burger.addEventListener('click', () => {
+	toggleMenu()
 })
+switchers.forEach(switcher => {
+	switcher.addEventListener('click', () => {
+		toggleTheme()
+	})
+})
+
+function toggleMenu() {
+	burger.classList.toggle('burger_active')
+	if (burger.classList.contains('burger_active')) {
+		headerMenu.classList.add('header__menu_active')
+	} else {
+		headerMenu.classList.remove('header__menu_active')
+	}
+}
+function toggleTheme() {
+	if (document.body.hasAttribute('light')) {
+		document.body.removeAttribute('light')
+		switchers.forEach(switcher => {
+			switcher.classList.remove('theme-light')
+		})
+	} else {
+		document.body.setAttribute('light', '')
+		switchers.forEach(switcher => {
+			switcher.classList.add('theme-light')
+		})
+	}
+}
 const swiper = new Swiper('.popular__swiper', {
 	sliderPerView: 2,
 	effect: 'fade',
