209870ef836321cd2c532578ca6617cd53531245
Андрей <martynkevich.2000@gmail.com>
2023-06-20 20:37:56 +0300

    refactor

--- project/src/components/popular/popular.js
+++ project/src/components/popular/popular.js
@@ -1,8 +1,8 @@
 const swiper = new Swiper('.popular__swiper', {
-	sliderPerView: 2,
-	effect: 'fade',
-	fadeEffect: {
-		crossFade: true
+	sliderPerView: 1,
+	spaceBetween: 20,
+	autoplay: {
+		delay: 5000
 	},
 	pagination: {
 		el: '.popular__dots',
